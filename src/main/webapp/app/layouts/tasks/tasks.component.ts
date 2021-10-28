86e8a94581251346720ce0c1401383989a784fc2
CsalaPeter <csalapeter95@gmail.com>
2021-10-28 13:38:18 +0200

    Wip

--- src/main/webapp/app/layouts/tasks/tasks.component.ts
+++ src/main/webapp/app/layouts/tasks/tasks.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Course } from '../courses/course.model';
+import { Subscription } from 'rxjs';
+import { CourseService } from 'app/layouts/courses/courses.service';
 
 @Component({
   selector: 'jhi-tasks',
   templateUrl: './tasks.component.html',
 })
-export class TasksComponent {
-  course?: Course;
+export class TasksComponent implements OnInit {
+  c: Course[] = [];
+  courseSubscription?: Subscription;
 
-  constructor() {}
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.courseSubscription = this.courseService.getAll().subscribe(course => (this.c = course));
+  }
 }
