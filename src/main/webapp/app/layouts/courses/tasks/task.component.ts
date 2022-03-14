843e4232f575e0f16bee0e58a81c161201e736f2
CsalaPeter <csalapeter95@gmail.com>
2022-03-14 13:58:55 +0100

    Gyors változtatások

--- src/main/webapp/app/layouts/courses/tasks/task.component.ts
+++ src/main/webapp/app/layouts/courses/tasks/task.component.ts
@@ -3,6 +3,8 @@ import { TaskModel } from 'app/models/task.model';
 import { Subscription } from 'rxjs';
 import { TasksService } from 'app/layouts/courses/tasks/tasks.service';
 import { ActivatedRoute } from '@angular/router';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'jhi-task',
@@ -10,6 +12,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['../../../../content/scss/layout/_task.scss'],
 })
 export class TaskComponent implements OnInit {
+  faCommentDots = faCommentDots;
+  faFile = faFile;
   task!: TaskModel;
   taskSubscription?: Subscription;
 
